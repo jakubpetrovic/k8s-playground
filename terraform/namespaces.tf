997a06877c9e14efabf87eab043bb49400717480
Jakub Petrovic <jakub.petrovic@webcom.cz>
2023-02-07 11:51:27 +0100

    update app namespaces

--- terraform/namespaces.tf
+++ terraform/namespaces.tf
@@ -4,14 +4,20 @@
   }
 } */
 
-resource "kubernetes_namespace" "helm-app" {
+/* resource "kubernetes_namespace" "helm-app" {
   metadata {
     name = "helm-app"
   }
+} */
+
+resource "kubernetes_namespace" "prod_ns" {
+  metadata {
+    name = "prod-ns"
+  }
 }
 
-resource "kubernetes_namespace" "test-app" {
+resource "kubernetes_namespace" "dev-ns" {
   metadata {
-    name = "test-app"
+    name = "dev-ns"
   }
 }
